098ecab9d0bbb70ee325f3960443d8472fcda002
fratzio <thomasmichaelfrazer@gmail.com>
2020-06-14 18:25:31 -0700

    Added vuematerial and boostrap | removed Helloworld component

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 
-Vue.config.productionTip = false
+Vue.use(VueMaterial);
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
